7ab48259a6b7f08a2e4d7d14f05e58506a36742c
jawad <jawad4545hamo@outlook.com>
2025-12-29 17:02:05 +0100

    create a routes tassociat each page to a route

---
+++ src/Controller/HelpdeskController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HelpdeskController extends AbstractController
+{
+    #[Route('/ticket/create', name: 'app_ticket_create')]
+    public function createTicket(): Response
+    {
+        return $this->render('create_ticket.html.twig');
+    }
+
+    #[Route('/agents', name: 'app_agents')]
+    public function agents(): Response
+    {
+        return $this->render('agents.html.twig');
+    }
+
+    #[Route('/user/create', name: 'app_user_create')]
+    public function createUser(): Response
+    {
+        return $this->render('adduser.html.twig');
+    }
+
+    #[Route('/ticket/assign', name: 'app_ticket_assign')]
+    public function assignTicket(): Response
+    {
+        return $this->render('ticket_assignement.html.twig');
+    }
+
+    #[Route('/attachments', name: 'app_attachments')]
+    public function attachments(): Response
+    {
+        return $this->render('attachements.html.twig');
+    }
+}
